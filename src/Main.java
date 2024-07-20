946357534193894f36e96fc1adf9e162af245edb
Devsco7 <dev.sco@outlook.com>
2024-07-20 16:44:09 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -16,15 +15,52 @@ public class Main {
     curso2.setDescricao("descrição curso js");
     curso2.setCargaHoraria(4);
 
+
+
     Mentoria mentoria = new Mentoria();
     mentoria.setTitulo("mentoria de java");
     mentoria.setDescricao("descrição mentoria java");
     mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devPedro = new Dev();
+        devPedro.setNome("Pedro");
+        devPedro.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Pedro" + devPedro.getConteudosInscritos());
+        devPedro.progredir();
+        devPedro.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Pedro" + devPedro.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Pedro" + devPedro.getConteudosConcluidos());
+        System.out.println("XP:" + devPedro.calcularTotalXp());
+
+        System.out.println("----------");
+
+        Dev devDamaris = new Dev();
+        devDamaris.setNome("Damaris");
+        devDamaris.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Damaris" + devDamaris.getConteudosInscritos());
+        devDamaris.progredir();
+        devDamaris.progredir();
+        devDamaris.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Damaris" + devDamaris.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Damaris" + devDamaris.getConteudosConcluidos());
+        System.out.println("XP:" + devDamaris.calcularTotalXp());
+
+
     }
 
 }
